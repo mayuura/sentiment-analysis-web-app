08c2b4fc7dc686e294ea6838e1bd551ba080bf60
mayuura <mehdi61049@gmail.com>
2024-08-18 20:05:43 +0200

    added some commetns

--- sentiment-analysis-app/src/App.js
+++ sentiment-analysis-app/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   const [result, setResult] = useState(null);
-
+  //function that gets triggered when submit button is clicked
   const handleSentimentSubmit = async (text) => {
     try {
       const response = await fetch('http://127.0.0.1:5000/analyze', {
--- sentiment-analysis-backend/app.py
+++ sentiment-analysis-backend/app.py
@@ -7,18 +7,22 @@ CORS(app)  # Enable CORS
 
 @app.route('/analyze', methods=['POST'])
 def analyze_sentiment():
+    #fetch the response in json format , its going to be a dictionary with several entries , and we want the value of key : text
     data = request.get_json()
+    #get the relevant text we want to perform our sentimetn analysis
     text = data.get('text', '')
 
+    #perform sentiment analysis on text 
     blob = TextBlob(text)
     sentiment = blob.sentiment
 
+    #create the response dictionary
     response = {
         'polarity': sentiment.polarity,
         'subjectivity': sentiment.subjectivity,
         'text': text
     }
-
+    #convert the dict to json format
     return jsonify(response)
 
 if __name__ == '__main__':
